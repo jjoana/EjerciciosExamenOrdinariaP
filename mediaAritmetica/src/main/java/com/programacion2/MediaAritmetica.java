a653dc6f424b9cb485b7339f56bf9fb4a1e9ce68
jjoana <jesus.joanaazuara@usp.ceu.es>
2023-05-29 13:34:16 +0200

    MediaAritmetica

--- mediaAritmetica/src/main/java/com/programacion2/MediaAritmetica.java
+++ mediaAritmetica/src/main/java/com/programacion2/MediaAritmetica.java
@@ -1,8 +1,22 @@
 package com.programacion2;
 
-public class MediaAritmetica {
-    
-    public int mediaAritmeticaR(int valor){
+import java.util.ArrayList;
+import java.util.List;
 
+public class MediaAritmetica {
+
+    List<Double> lista = new ArrayList<Double>();
+
+    public double mediaAritmeticaR(int tamano, List<Double> miLista) {
+        double valor = 0;
+
+        if (!miLista.isEmpty()) {
+            valor = miLista.get(0) / tamano;
+            miLista.remove(0);
+            if (tamano > 1) {
+                valor += mediaAritmeticaR(tamano, miLista);
+            }
+        }
+        return valor;
     }
 }
--- mediaAritmetica/src/test/java/com/programacion2/MediaAritmeticaTest.java
+++ mediaAritmetica/src/test/java/com/programacion2/MediaAritmeticaTest.java
@@ -1,5 +1,47 @@
 package com.programacion2;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+
 public class MediaAritmeticaTest {
-    
+
+    private MediaAritmetica objeto;
+
+    @BeforeEach
+    void crearObjeto() {
+        objeto = new MediaAritmetica();
+    }
+
+    @Test
+    void mediaAritmeticaTestR() {
+        List<Double> lista = new ArrayList<Double>();
+        lista.add(1.0);
+        lista.add(2.0);
+        lista.add(3.0);
+        assertEquals(1.99, objeto.mediaAritmeticaR(3, lista), 0.01);
+    }
+
+    @Test
+    void mediaAritmeticaTestR1() {
+        List<Double> lista = new ArrayList<Double>();
+        lista.add(5.0);
+        lista.add(5.0);
+        lista.add(5.0);
+        lista.add(5.0);
+        lista.add(5.0);
+        assertEquals(5.0, objeto.mediaAritmeticaR(5, lista), 0.01);
+    }
+
+    @Test
+    void mediaAritmeticaTestR2() {
+        List<Double> lista = new ArrayList<Double>();
+        lista.add(0.0);
+        assertEquals(0, objeto.mediaAritmeticaR(1, lista), 0.01);
+    }
+
 }
