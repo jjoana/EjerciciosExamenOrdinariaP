b266bf7a318d01eb0aec636d2401fd4b317ab9a3
jjoana <jesus.joanaazuara@usp.ceu.es>
2023-05-31 12:14:45 +0200

    Potencia termianada

--- potencia/src/main/java/com/programacion2/Potencia.java
+++ potencia/src/main/java/com/programacion2/Potencia.java
@@ -2,21 +2,31 @@ package com.programacion2;
 
 public class Potencia {
 
-    public int potenciaRecursivo(int base, int exponente){
+    public int potenciaRecursivo(int base, int exponente)throws MiExcepcion{
         int resultado = base;
-        if(exponente > 0){
+        if(exponente < 0){
+            throw new MiExcepcion();
+        } else  if (exponente > 0){
             resultado*= potenciaRecursivo(base, exponente-1);
-        } else {
-            return 1;
+        } else { // caso base recurisvo
+            resultado =1;
         }
         return resultado;
     }
 
-    public int potenciaIterativo(int base, int exponente){
+    public int potenciaIterativo(int base, int exponente) throws MiExcepcion{
         int resultado = 1;
-        for(int i = exponente; i>0; i--){
-            resultado *= base;
+        if(exponente <0){
+            throw new MiExcepcion();
+        } else {
+            for(int i = exponente; i>0; i--){
+                resultado *= base;
+            }
+            return resultado;
         }
-        return resultado;
     }
 }
+
+class MiExcepcion extends Exception {
+    
+} 
--- potencia/src/test/java/com/programacion2/PotenciaTest.java
+++ potencia/src/test/java/com/programacion2/PotenciaTest.java
@@ -1,6 +1,8 @@
 package com.programacion2;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,37 +16,44 @@ public class PotenciaTest {
     }
 
     @Test
-    void potenciaR() {
+    void potenciaR() throws MiExcepcion {
         assertEquals(16, objeto.potenciaRecursivo(2, 4));
     }
 
     @Test
-    void potenciaR2() {
+    void potenciaR2()throws MiExcepcion {
         assertEquals(2, objeto.potenciaRecursivo(2, 1));
     }
 
     @Test
-    void potenciaR3() {
+    void potenciaR3()throws MiExcepcion  {
         assertEquals(1, objeto.potenciaRecursivo(2, 0));
      }
 
     @Test
-    void potenciaR4() {
+    void potenciaR4() throws MiExcepcion {
         assertEquals(0, objeto.potenciaRecursivo(0, 2));
      }
 
+     @Test
+    void potenciaR5() throws MiExcepcion{
+        assertThrows(MiExcepcion.class, () ->{
+            objeto.potenciaRecursivo(0, -1);
+        });
+    }
+
     @Test
-    void potenciaI1() {
+    void potenciaI1() throws MiExcepcion {
         assertEquals(0, objeto.potenciaIterativo(0, 2));
      }
 
      @Test
-    void potenciaI2() {
+    void potenciaI2() throws MiExcepcion {
         assertEquals(1, objeto.potenciaIterativo(2, 0));
      }
 
      @Test
-    void potenciaI3() {
+    void potenciaI3() throws MiExcepcion {
         assertEquals(16 , objeto.potenciaIterativo(2, 4));
-     }
+     }  
 }
